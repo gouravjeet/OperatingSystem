a628f052df2336dcc73771297c89be97bb550ad9
gouravjeet <gouravjeet@gmail.com>
2014-09-23 03:41:15 -0400

    adding exclamationa nd redirection

--- Assignment1/Ex2.c
+++ Assignment1/Ex2.c
@@ -106,28 +106,7 @@ void cdProcessing(char *args[100],char paths[100],int args_len){
 	else
 		printf("%s Directory doesnot exists\n",args[1]);
 }
-void runExclamationCommand(char *args[100],char paths[100],int args_len,char history[1000][1000]){
-	// run the command with ! followed either by the name of command or by the number of the command
-	printf("%s\n","run exclamation" );
-	char command[100];
-	char cmd[100];
-	sscanf(args[0] ,"!%s ", command);
-	printf("%s\n", command);
-	if(isdigit(command[0])){								// integer
-		int num = atoi(command);
-		printf("%d\n", num);
-		//args[0]=historyChecking(history);
-		forkMethod(args,paths,args_len);
-	}
-	else if(isalpha(command[0])){								//string
-		//printf("%s\n", command);
-		args[0]=command;
-		printf("%s\n",args[0]);
-		printf("%d\n", args_len);
-		
-		forkMethod(args,paths,args_len);
-	}	
-}
+
 void historyChecking(char history[1000][1000], int *history_index,char *choice,bool historyflag){
 	
 	// History is calculated here, so at each step, cmd are added to the history array so that we can get that later on
@@ -145,25 +124,9 @@ void historyChecking(char history[1000][1000], int *history_index,char *choice,b
 
 }
 void PreArgsProcessing(char *args[100],char paths[100],int *args_len,char *choice,char *args_temp){
-			//printf("This is Choice:   -------------<<<<>>>>>>>>>>><<<<<<>>>>> %s\n", choice);
-			printf("%d\n", *args_len);
-			// for(int i=0; i<*args_len-1; i++){
-			// 	memset(args[i], ' ', strlen(args[i]));	
-			// 	printf("LOLLLL : %s\n",args[i]);
-			// }
-			// printf("Mile jab hum tum\n");
+			//printf("%d\n", *args_len);
 			*args_len = 0;
-			//printf("Mile jab hum tum\n");						
-			// Different arguments are prepared by tokenising that can be used later
-			//choice = trimwhitespace(choice);
-			//char pch = memchr (choice, '\n', strlen(choice));
-			//printf("MILA hai kuch              ---                   -----------            >>>>>          %c", pch);
-			//pch = ' ';
-			//int z = sizeof(choice)/sizeof(char);
-			//choice[z-1] = ' ';
-			//choice[z] = ' ';
-			//printf("This is the sizeof Choice :                  -------------------------          >>>>>>>>>>%d\n", z);
-			printf("Here is CHoice :    \n\n\n\n Choice %s", choice);
+			//printf("Here is CHoice :    \n\n\n\n Choice %s", choice);
 			args_temp=strtok(choice," ");
 			while(args_temp != NULL){
 				args[*args_len] = args_temp;		
@@ -173,16 +136,48 @@ void PreArgsProcessing(char *args[100],char paths[100],int *args_len,char *choic
 			//args[*args_len-1] = trimwhitespace(args[*args_len-1]);
 			args[*args_len] = NULL;
 			*args_len=*args_len+1;
-			printf("Length of all the args :  ------------------>>>     %d\n", *args_len);
+			//printf("Length of all the args :  ------------------>>>     %d\n", *args_len);
 			for(int i=0; i<*args_len; i++){
 				printf("These are args: %d %s\n", i, args[i]);
 			}
+}
+void runExclamationCommand(char *args[100],char paths[100],int args_len,char history[1000][1000],char *choice,char *args_temp){
+	// run the command with ! followed either by the name of command or by the number of the command
+	char temp4[1000];
+	memset(temp4, ' ', 1000);
+	printf("%s\n","run exclamation" );
+	char command[100];
+	char cmd[100];
+	sscanf(args[0] ,"!%s ", command);
+	printf("%s\n", command);
+	if(isdigit(command[0])){								// integer
+		int num = atoi(command);
+		printf("%d\n", num);
+		//args[0]=historyChecking(history);
+		
+		forkMethod(args,paths,args_len);
+	}
+	else if(isalpha(command[0])){								//string
+		//printf("%s\n", command);
+		args[0]=command;
+		//printf("%s\n",args[0]);
+		strcpy(temp4,args[0]);
+		printf("%d\n", args_len);
+		for(int o=0;0<args_len-2;o++){
+			strcat(temp4,args[o]);
+			strcat(temp4," ");
+		}
+		strcpy(choice,temp4);
+		choice=trimwhitespace(choice);
+		PreArgsProcessing(args,paths,&args_len,choice,args_temp);
+		//forkMethod(args,paths,args_len);
+	}	
 }
 void BackgroundProcesses(char *args[100],char paths[100],int args_len){
 	printf("background process");
 	// user should execute a process in the background if user enters & at the end of the command
 }
-void Redirection(char *args[100],char paths[100],int args_len,char *choice,char *args_temp){
+void Redirection(char *args[100],char paths[100],int args_len,char *choice,char *args_temp, int *flag_to){
 	// Redirect the stdin and stdout to a file
 	// case 1: cmd1 < stdout.text  stdout is input to cmd1 
 	     // redirects data from stdout to stdin for cmd1
@@ -190,60 +185,40 @@ void Redirection(char *args[100],char paths[100],int args_len,char *choice,char 
 		 // open a file with fd 0
 		 // child process and by default it reads from stdin ie fd-0
 	char temp2[1000];
+	char temp3[1000];
 	char *pn;
 	memset(temp2, ' ', 1000);
+	memset(temp3, ' ', 1000);
 	char temp_buffer[100];
 	printf("%d\n", args_len);
+	printf("dara" );
 	for(int i=0;i<args_len-1;i++){
-		//printf("starting\n");
-		//printf("%s %d for loop starting",i);
 		if(strncmp(args[i],"<",1)==0){
 			printf("Redirect from file.\n");
 			//int fd_stored=dup(0);
 			//close(0);
 			int fname;
-			
-			//printf("%s",args[i+1] );
 			args[i+1] = trimwhitespace(args[i+1]);
 			fname=open(args[i+1],O_RDONLY,0660); 					//filename
 			char temp_buffer[1000];
 			int fileSize=read(fname,temp_buffer,100);
 			temp_buffer[fileSize] = '\0';
-			//fflush(stdin);
-			//fflush(fname);
-			///fflush(stdout);
-			printf("Here I am, this is me: %sHello\n\n\n\n\n\n\n\n\n", temp_buffer);
-			printf("%d\n", fileSize);
 			memcpy ( temp_buffer, temp_buffer, fileSize );
 			char* temp[100];
-			
 			// create a copy of args and then update it with the new value --> cmd1 args and then run it using execv
 			//strcpy(args[0],temp);
 			strcpy(temp2, args[0]);
 			printf("%d\n", i);
 			for(int j=1;j<i;j++){
-				printf("Aayush Sir is the greatest person:    %s \n", args[j]);
 				strcat(temp2," ");
 				strcat(temp2,args[j]);
 			}
-			printf(" Aayush was here, fuck yeah!!%shelloooooooooooooooooooo", temp_buffer);
 			strcat(temp2,temp_buffer);
-			printf(" Aayush was here, fuck yeah!! %s\n\n\n\n\n\n\n\n helloooooooooooooooooooo", temp2);
 			strcpy(choice,temp2);
-			//printf(" Aayush was here, fuck yeah!!   %s helloooooooooooooooooooo", temp_buffer);
 			char * a = trimwhitespace(temp_buffer);
-			//a=trimwhitespace(a);
-			//printf(" Aayush was here, fuck yeah!!   %s hello", a);
 			choice = trimwhitespace(choice);
 
-			// memset(temp_buffer, ' ', 1000);
-			// int z;
-			// for(z=0; a[z] != '/0'; z++){
-			// 	temp_buffer[z] = a[z];
-			// 	printf("a\n");
-			// }
-			// temp_buffer[z] = '/0';
-			printf("New Sardar : %s", choice);
+			
 			PreArgsProcessing(args,paths,&args_len,choice,args_temp);
 			//printf(" temp2 :: %s  a value %s choice : %s", temp2, a, choice);
 			//pn=checkingParamter(args,paths,args_len,path,dir,path_len,file,pn);
@@ -258,31 +233,101 @@ void Redirection(char *args[100],char paths[100],int args_len,char *choice,char 
 			// for(int k=i;k<args_len-2;k++){   //set arguments after < equal to null
 			// 	args[k]=NULL;
 			// }
-			strncmp(args[i],">>",2);
+			//strncmp(args[i],">>",2);
 			printf("breaking\n");
-			//dup2(fd_stored, 0);
 			return;
-			// exit(0);
-			
 		}
 		//printf("ss\n");
 			// case 2: cmd1 >> stdout.txt  append the response of the command 1 to teh text file
 		else if(strncmp(args[i],">>",2)==0){
 			printf("Append to file");
-			break;
+			*flag_to=1;
+			int dv=dup(1);
+			char * b;
+			
+			//close(1);
+			open(args[i+1],O_RDWR | O_CREAT, 0660);
+			pid_t pid;  
+			//printf( "abc");
+			// strcpy(temp3, args[0]);
+			// strcat(temp3, " ");
+			for(int m=0;m<i-1;m++){
+				printf("argument are %s\n",args[m] ); 
+				strcat(temp3, args[m]);
+				strcat(temp3, " ");
+			}
+			strcat(temp3, args[i-1]);
+			strcpy(choice,temp3);
+			choice = trimwhitespace(choice);
+			printf("%s\n", choice);
+			PreArgsProcessing(args,paths,&args_len,choice,args_temp);	  
+			pid = fork(); 																	//forking a process
+			if(pid<0){
+				printf("error !!");
+			}	
+			if(pid == 0){
+				printf("HCild\n");
+				for(int i=0; i<args_len; i++){
+					if(args[i]!=NULL){
+						args[i] = trimwhitespace(args[i]);
+					}
+				}
+				execv(paths, args);
+			}
+			else{
+				printf("parent\n");
+				int status;
+				int termChild = wait(&status);
+				//printf("terminated");
+				dup2(dv,1);
+			}
+			return;								
 		}
 			// case 3 : cmd2 > stdout.text  override the response of the command 1 to the text file
 			// paths[0] paths[1] paths[2]
 			     // first release fd 1 and then open a file with fd 1 and then open a child process and 
 			     // then the program writes to stdout ie fd 1 and hence in this case it is file 
-		else if(strncmp(args[i],">",1)==0){
+		else if(strncmp(args[i],">",1)==0){						
+			*flag_to=1;
 	 		printf("Redirect to file");
-	 		break;
-	 		// close(1);
-	 		// //open(args[2],"w");
-	 		// if(fork()==0){ // child process
-	 		// //	execv();
-	 		// }
+			int dv=dup(1);
+			char * b;
+			
+			//close(1);
+			open(args[i+1],O_RDWR | O_CREAT, 0660);
+			pid_t pid;  
+			strcpy(temp3, args[0]);
+			strcat(temp3, " ");
+			for(int m=1;m<i;m++){
+				printf("argument are %s\n",args[m] ); 
+				strcat(temp3, args[m]);
+				strcat(temp3, " ");
+			}
+			strcpy(choice,temp3);
+			choice = trimwhitespace(choice);
+			printf("%s\n", choice);
+			PreArgsProcessing(args,paths,&args_len,choice,args_temp);	  
+			pid = fork(); 																	
+			if(pid<0){
+				printf("error !!");
+			}	
+			if(pid == 0){
+				printf("HCild\n");
+				for(int i=0; i<args_len; i++){
+					if(args[i]!=NULL){
+						args[i] = trimwhitespace(args[i]);
+					}
+				}
+				execv(paths, args);
+			}
+			else{
+				printf("parent\n");
+				int status;
+				int termChild = wait(&status);
+				//printf("terminated");
+				dup2(dv,1);
+			}
+		return;								
 		}
 		printf("Hereend" );	
 	}
@@ -353,23 +398,33 @@ void historyPrinting(char history[1000][1000],int history_index, int numbering_i
 	   		}
 }
 int main(){
-   		char *choice;           			// choice is the first argument
+             			// choice is the first argument
    		char *path[100],*path_temp;
-   		char *args[100], *args_temp;
+   		char *args_temp;
    		char history[1000][1000];
    		char *newpath;
    		bool historyflag=false;
    		char paths[100];
-   	 	int flag = 0;
+   	 	
    	 	int ttoo = 1;
    		int history_index=0,numbering_index=0;
    		DIR * dir[100];
+   		int f=0;
    		int args_len =0, path_len=0;
 	    while(1){
-	    	flag=0;																		//infinite while loop starting
+	  //   	while(args[f]!=NULL){   // Setting the default value after each iteration as null
+			// 	args[f]=NULL;
+			// 	printf("%s\n", args[f]);
+			// 	f++;
+			// }
+			char *choice=malloc(1000);
+			char *args[100];
+	    	int flag = 0;
+   	 		int flag_to=0;																		//infinite while loop starting
 			args_len = 0;
 			path_len=0;
-			printf("sardarKaShell-1.0-$ ");
+
+			printf(">>>>>>>");
 			path_temp=getenv("MYPATH");
 			path_temp=strtok(path_temp,":");
 			
@@ -380,7 +435,13 @@ int main(){
 			   	path_temp = strtok(NULL,":");
 			}
 			int s;
+			//printf("%s before\n",choice);
 			fgets(choice,1000,stdin);
+			choice=trimwhitespace(choice);
+			// if(choice==){
+			// 	continue;
+			// }
+			//printf("%s\n",choice);
 			PreArgsProcessing(args,paths,&args_len,choice,args_temp);
 			historyChecking(history,&history_index,choice, historyflag);
 			struct dirent * file;
@@ -395,25 +456,33 @@ int main(){
 				printf("bye\n");
 				exit(0);
 			}
+			// if(strncmp(args[args_len-2],"&",1)==0){		
+   //  			printf( "found %s\n", args[args_len-2] );									//Background command 
+			// 	BackgroundProcesses(args,paths,args_len);
+			// 	flag=1;
+			// 	break;	
+			// }
+			// 												// checking for paramater to verify the arguments                     
+		 // 	else if(strncmp(args[0],"cd",2)==0){		
+	  //   			//printf( "found %s\n", args[1] );									// cd command 
+			// 		cdProcessing(args,paths,args_len);
+			// 		break;	
+			// }
 			// checking for various commands
 			for(int i=0;i<args_len-1;i++){
-				if(strncmp(args[i],"<",1)==0 || strncmp(args[i],">",1)==0 || strncmp(args[i],">>",2)==0){  // redirection
-					printf("value Aayush of i %d\n",i );	
-					Redirection(args,paths,args_len,choice,args_temp);
-					printf("Hereback" );	
-					flag=1;
-					// 
-					//ttoo = 0;
-					//exit(0);
-					break;
-				}
-				else if(strncmp(args[i],"!",1)==0){		
+				if(strncmp(args[i],"!",1)==0){		
 	    			printf( "found %s\n", args[0] );									// Exclamation command 
-					runExclamationCommand(args,paths,args_len,history);
+					runExclamationCommand(args,paths,args_len,history,choice,args_temp);
 					flag=1;
 					break;	
 				}
-				else if(strncmp(args[i],"|",1)==0){		
+				if(strncmp(args[i],"<",1)==0 || strncmp(args[i],">",1)==0 || strncmp(args[i],">>",2)==0){  // redirection
+					Redirection(args,paths,args_len,choice,args_temp,&flag_to);
+					flag=1;
+					break;
+				}
+				
+				if(strncmp(args[i],"|",1)==0){		
 					//printf( "found %s\n", args[1] );									// pipe command 
 					IPCPipes(args,paths,args_len);
 					flag=1;
@@ -421,19 +490,8 @@ int main(){
 				}
 			//printf("%d\n",i);
 			}
-			// if(strncmp(args[args_len-2],"&",1)==0){		
-   //  			printf( "found %s\n", args[args_len-2] );									//Background command 
-			// 	BackgroundProcesses(args,paths,args_len);
-			// 	ttoo=1;
-			// 	continue;	
-			// }
-			// 												// checking for paramater to verify the arguments                     
-		 // 	else if(strncmp(args[0],"cd",2)==0){		
-	  //   			//printf( "found %s\n", args[1] );									// cd command 
-			// 		cdProcessing(args,paths,args_len);
-			// 		continue;	
-			// }
-			if(flag){
+			
+			if(flag & !flag_to){
 				// the commands left at the end will be executed
 				//printf("%d\n",flag );
 				newpath=checkingParamter(args,paths,args_len,path,dir,path_len,file,newpath);
@@ -443,9 +501,21 @@ int main(){
 				else {
 					forkMethod(args,paths,args_len);
 				}	
-			}	
-			for(int i=0;args_len-2;i++){   // Setting the default value after each iteration as null
-				args[i]=NULL;
-			}	 
+			}
+			if(!flag & !flag_to){
+				// the commands left at the end will be executed
+				//printf("%d\n",flag );
+				newpath=checkingParamter(args,paths,args_len,path,dir,path_len,file,newpath);
+				//printf("%s\n", newpath);
+				if(newpath==NULL){
+				}
+				else {
+					forkMethod(args,paths,args_len);
+				}	
+			}		
+			free(choice);
+			// for(int i=0;args_len-2;i++){   // Setting the default value after each iteration as null
+			// 	args[i]=NULL;
+			// }	 
    }//while(1)
 }// main 
